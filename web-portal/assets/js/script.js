b2939068e35865494fa5d08719b0da995e1edfef
Matthias Krijgsman <matthiaskrijgsman@Matthiass-MacBook-Air.local>
2017-01-25 14:36:09 +0100

    Finished main styling

--- web-portal/assets/js/script.js
+++ web-portal/assets/js/script.js
@@ -1,5 +1,56 @@
 /* Author: Matthias Krijgsman */
 
+
 $(document).ready(function(){
     $(".login-container").css('left', '0');
 });
+
+$(".weerrapport-menu > li").click(function(){
+    resetTabView();
+    $(this).addClass('active');
+    $(".single-tab[tab='"+$(this).attr('tab')+"']").addClass('active');
+});
+
+function resetTabView(){
+    $(".single-tab").each(function(){
+        $(this).removeClass('active');
+    });
+    $(".weerrapport-menu > li").each(function(){
+        $(this).removeClass('active');
+    });
+}
+
+
+// Veranderen Waterstand in CM:
+function veranderWaterstand(cm){
+    $("#waterstand").text(parseFloat(cm)/100);
+}
+
+// Veranderen Status Server
+function veranderServerStatus(server, status){
+    if(server == 1){
+        if(status){
+            $("#serverstatus1").attr('src', '../assets/img/server-on.png');
+            $("#server1").text('Server Online');
+            $("#server1").addClass('server-on');
+            $("#server1").removeClass('server-off');
+        }else{
+            $("#serverstatus1").attr('src', '../assets/img/server-off.png');
+            $("#server1").text('Server Offline');
+            $("#server1").addClass('server-off');
+            $("#server1").removeClass('server-on');
+        }
+    }else{
+        if(status){
+            $("#serverstatus2").attr('src', '../assets/img/server-on.png');
+            $("#server2").text('Server Online');
+            $("#server2").addClass('server-on');
+            $("#server2").removeClass('server-off');
+        }else{
+            $("#serverstatus2").attr('src', '../assets/img/server-off.png');
+            $("#server2").text('Server Offline');
+            $("#server2").addClass('server-off');
+            $("#server2").removeClass('server-on');
+        }
+    }
+}
--- web-portal/dashboard/index.php
+++ web-portal/dashboard/index.php
@@ -1,1 +1,83 @@
 <?php
+/* Author: Matthias Krijgsman */
+require_once '../config.php';
+require_once '../sessionController.php';
+
+?>
+
+<html>
+
+<head>
+    <title>Maeslantkering - Web Portaal</title>
+    <link rel="stylesheet" href="../assets/css/style.css">
+</head>
+
+<body class="dashboard">
+    <div class="dashboard-container">
+
+        <div class="card card-small">
+            <div class="card-title">Server 1 status</div>
+            <img id="serverstatus1" src="../assets/img/server-on.png" class="server-icon">
+            <label id="server1" class="server-status server-on">Server Online</label>
+        </div>
+
+        <div class="card card-small">
+            <div class="card-title">Server 2 status</div>
+            <img id="serverstatus2" src="../assets/img/server-off.png" class="server-icon">
+            <label id="server2" class="server-status server-off">Server Offline</label>
+        </div>
+
+        <div class="card card-medium">
+            <div class="card-title">Sensor 1 status</div>
+            <div class="waterstand-container">WATERSTAND: <label id="waterstand">12.53</label><label> meter</label></div>
+        </div>
+
+        <div class="card card-small buienradar">
+            <div class="card-title">Weerrapporten</div>
+            <ul class="weerrapport-menu">
+                <li tab="0" class="active">Windsnelheid</li>
+                <li tab="1">Windkracht</li>
+                <li tab="2">Windstoten</li>
+                <li tab="3">Zicht</li>
+                <li tab="4">Buienradar</li>
+                <li tab="5">Temperatuur</li>
+                <li tab="6">Gevoelstemperatuur</li>
+            </ul>
+        </div>
+
+        <div class="card card-medium buienradar">
+            <div class="card-title">Weerrapport</div>
+            <div class="weerrapport-container">
+                <div class="single-tab active" tab="0"><img src="http://cdn.knmi.nl/knmi/map/page/weer/actueel-weer/windsnelheid.png"></div>
+                <div class="single-tab" tab="1"><img src="http://cdn.knmi.nl/knmi/map/page/weer/actueel-weer/windkracht.png"></div>
+                <div class="single-tab" tab="2"><img src="http://cdn.knmi.nl/knmi/map/page/weer/actueel-weer/maxwindkm.png"></div>
+                <div class="single-tab" tab="3"><img src="http://cdn.knmi.nl/knmi/map/page/weer/actueel-weer/zicht.png"></div>
+                <div class="single-tab" tab="4"><iframe class="buienradar-info" SRC="http://api.buienradar.nl/image/1.0/RadarMapNL?w=460&h=502" NORESIZE SCROLLING=NO HSPACE=0 VSPACE=0 FRAMEBORDER=0 MARGINHEIGHT=0 MARGINWIDTH=0 WIDTH=460 HEIGHT=502></iframe></div>
+                <div class="single-tab" tab="5"><img src="http://cdn.knmi.nl/knmi/map/page/weer/actueel-weer/temperatuur.png"></div>
+                <div class="single-tab" tab="6"><img src="http://cdn.knmi.nl/knmi/map/page/weer/actueel-weer/gevoelstemperatuur.png"></div>
+            </div>
+        </div>
+
+        <div class="card card-small buienradar">
+            <div class="card-title">Informatie</div>
+            <ul class="informatie-lijst">
+                <li><label>Status deuren:</label></li>
+                <li><div class="informatie-status">Open</div></li>
+                <li><label>Weer:</label></li>
+                <li><div class="informatie-status">Normaal</div></li>
+                <li><label>Waterstand:</label></li>
+                <li><div class="informatie-status">Normaal</div></li>
+                <li style="margin-top: 40px;"><label>Handmatig Sluiten:</label></li>
+                <li><div class="button-close-open">Sluiten</div></li>
+            </ul>
+        </div>
+
+
+
+    </div>
+</body>
+
+<script src="../assets/js/jq.js"></script>
+<script src="../assets/js/script.js"></script>
+
+</html>
